1844a6c2992b4d7de10c909dc3b8a69b71f8d11d
dl6544 <michiel.vanhaegenborgh@engie.com>
2025-11-18 22:47:05 +0100

    initial

---
+++ providers.tf
@@ -0,0 +1,30 @@
+terraform {
+  backend "remote" {
+    hostname     = "stackweaver.truyens.pro"
+    organization = "default"
+
+    workspaces {
+      name = "default"
+    }
+  }
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = ">=6.0.0,<7.0.0"
+#     }
+#   }
+}
+
+# provider "aws" {
+#   profile    = var.AWS_PROFILE
+#   region     = var.AWS_REGION
+#   access_key = var.AWS_ACCESS_KEY_ID
+#   secret_key = var.AWS_SECRET_ACCESS_KEY
+
+#   dynamic "assume_role" {
+#     for_each = var.AWS_ASSUME_ROLE_ARN != null ? [1] : []
+#     content {
+#       role_arn = var.AWS_ASSUME_ROLE_ARN
+#     }
+#   }
+# }
