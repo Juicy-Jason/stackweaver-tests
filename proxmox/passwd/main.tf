02c1f3f4123007a51fbd76f4c32e3b16febc99df
michielvha <michielvh@outlook.com>
2025-12-30 22:34:14 +0100

    chore: trying out proxmox passwd auth since api key auth is giving issues

---
+++ proxmox/passwd/main.tf
@@ -0,0 +1,16 @@
+# # Simple data source to test Proxmox connectivity - using version for simplicity
+# data "proxmox_virtual_environment_version" "version" {}
+
+# Simple resource creation - add a hosts entry
+resource "proxmox_virtual_environment_hosts" "test_entry" {
+  node_name = "test-node"
+  entry {
+    address  = "192.168.1.100"
+    hostnames = ["test-terraform.local"]
+  }
+}
+
+# # Output the version to verify it works
+# output "proxmox_version" {
+#   value = data.proxmox_virtual_environment_version.version
+# }
---
+++ proxmox/passwd/variables.tf
@@ -0,0 +1,22 @@
+variable "proxmox_endpoint" {
+  description = "Proxmox API endpoint URL"
+  type        = string
+}
+
+variable "proxmox_insecure" {
+  description = "Skip TLS verification"
+  type        = bool
+  default     = true
+}
+
+variable "proxmox_username" {
+  description = "Proxmox username"
+  type        = string
+  default     = "root@pam"
+}
+
+variable "proxmox_password" {
+  description = "Proxmox password"
+  type        = string
+  sensitive   = true
+}
