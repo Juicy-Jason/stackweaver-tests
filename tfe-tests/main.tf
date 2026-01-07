caa680d14373f3c45b4ba72ca54b085843101089
michielvha <michielvh@outlook.com>
2026-01-07 18:00:18 +0100

    chore: aliging some values with current context

--- tfe-tests/main.tf
+++ tfe-tests/main.tf
@@ -15,11 +15,11 @@ data "tfe_workspace" "test" {
 
 // https://registry.terraform.io/providers/hashicorp/tfe/latest/docs/resources/team_access#argument-reference
 // Test 1: Fixed access level - read
-# resource "tfe_team_access" "test_team_access_read" {
-#   access       = "read" # Valid: read, plan, apply, write, admin, none
-#   team_id      = tfe_team.test_team.id
-#   workspace_id = data.tfe_workspace.test.id
-# }
+resource "tfe_team_access" "test_team_access_read" {
+  access       = "read" # Valid: read, plan, apply, write, admin, none
+  team_id      = tfe_team.test_team.id
+  workspace_id = data.tfe_workspace.test.id
+}
 
 
 resource "tfe_team_access" "test_team_access_custom" {
