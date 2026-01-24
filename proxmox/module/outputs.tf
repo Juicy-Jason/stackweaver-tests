2ede92d79f7e6f3d25e9c790287fba6bdbf16bf3
michielvha <michielvh@outlook.com>
2026-01-24 21:07:00 +0100

    fix: syntax error

--- proxmox/module/outputs.tf
+++ proxmox/module/outputs.tf
@@ -6,7 +6,9 @@ output "proxmox_version" {
 output "iso_file_path" {
   description = "Path to the downloaded ISO file"
   value       = proxmox_virtual_environment_download_file.test_iso.id
-}output "pool_id" {
+}
+
+output "pool_id" {
   description = "ID of the created pool"
   value       = proxmox_virtual_environment_pool.test_pool.pool_id
 }
