526209306f5cf2e428141594fe8619ab6fcb6273
michielvha <michielvh@outlook.com>
2026-01-24 22:51:13 +0100

    chore: trying out some stuffs

--- proxmox/module/variables.tf
+++ proxmox/module/variables.tf
@@ -4,18 +4,8 @@ variable "node_name" {
   default     = "pve"
 }
 
-# this url changes each time there is a new stable so go to https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/ and copy the link from there if the one provided no longer works
-variable "iso_url" {
-  description = "URL of the ISO file to download"
-  type        = string
-  default     = "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-13.2.0-amd64-netinst.iso"
-}
-
-variable "iso_filename" {
-  description = "Filename for the downloaded ISO"
-  type        = string
-  default     = "debian-13.2.0-amd64-netinst.iso"
-}
+# Latest Debian amd64 netinst ISO URL and filename are resolved at plan time
+# via get-debian-netinst-url.sh (fetches cdimage directory listing; works for 13.x, 14.x, etc.).
 
 variable "pool_id" {
   description = "Pool ID for the test pool"
