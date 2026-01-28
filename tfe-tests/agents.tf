92b8d3b20738537841a2d925a0816c4c0393d5d7
michielvha <michielvh@outlook.com>
2026-01-28 22:44:32 +0100

    chore: testing out some tfe provider resources

---
+++ tfe-tests/agents.tf
@@ -0,0 +1,25 @@
+resource "tfe_organization" "test-organization" {
+  name  = "stackweaver-tests-tfe-provider"
+  email = "admin@@vhco.pro"
+}
+
+resource "tfe_agent_pool" "test-agent-pool" {
+  name         = "my-agent-pool-name"
+  organization = "main"
+  organization_scoped = true
+}
+
+data "tfe_project" "default-project" {
+  name         = "default-provider"
+  organization = "main"
+}
+
+resource "tfe_agent_pool_allowed_projects" "allowed_projects" {
+  agent_pool_id         = tfe_agent_pool.test-agent-pool.id
+  allowed_project_ids   = [data.tfe_project.default-project.id]
+}
+
+# resource "tfe_agent_pool_allowed_workspaces" "allowed_workspaces" {
+#   agent_pool_id         = tfe_agent_pool.test-agent-pool.id
+#   allowed_workspace_ids = [data.tfe_workspace.test-workspace.id]
+# }
